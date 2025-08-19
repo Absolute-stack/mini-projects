d8b981d36e9565c1176dfa62fb7d27e53bd66d7c
imdeabsolute@gmail.com <imdeabsolute@gmail.com>
2025-08-19 15:07:50 +0000

    Complete simple color flipper with html css javascript and jest for test-driven development

--- hex.js
+++ hex.js
@@ -1,3 +1,10 @@
-for (let i = 999999; i >= 0; i - Math.random() * 1000) {
-  console.log(`#${i}`);
+const btn = document.querySelector('.hex-btn');
+btn.addEventListener('click', () => {
+  document.documentElement.style.setProperty('--color', colorGenerator());
+  const hex = document.querySelector('.hex-code');
+  hex.textContent = colorGenerator();
+});
+
+function colorGenerator() {
+  return `#${Math.floor(Math.random() * 999999)}`;
 }
